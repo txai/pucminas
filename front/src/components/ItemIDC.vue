894cc8a92605543bb6584e0a76b59ade00dd6edd
Txai <txai.garcia@gmail.com>
2022-09-28 09:45:01 -0300

    Lista de IDC com filtro

---
+++ front/src/components/ItemIDC.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+    <v-row>
+        <v-col xs="12">
+        <v-card>
+            <v-card-title>{{ idc.nome }}</v-card-title>
+            <v-card-subtitle>{{ idc.dataDistribuicao }}</v-card-subtitle>
+        </v-card>
+        </v-col>
+    </v-row>
+</v-container>
+</template>
+
+<script>
+    export default {
+        name: "ItemIDC",
+        data() {
+            return {}
+        },
+        props: {
+            idc: Object
+        }
+    }
+</script>
--- front/src/views/MinhasIDCs.vue
+++ front/src/views/MinhasIDCs.vue
@@ -1,18 +1,48 @@
 <template>
-    <v-main>
-        <v-autocomplete>Buscar</v-autocomplete>
-        <ListaIDC></ListaIDC>
-    </v-main>
+    <v-container>
+        <v-data-iterator
+            :items="idcs"
+            :search="search"
+        >
+            <template v-slot:header>
+                <v-text-field
+                    v-model="search"
+                    label="Buscar"
+                    clearable
+                    flat
+                    hide-details="auto"
+                ></v-text-field>
+            </template>
+            <template v-slot:default="props">
+                <v-list>
+                    <v-list-item v-for="item in props.items" :key="item.nome">
+                        <ItemIDC :idc="item"></ItemIDC>
+                    </v-list-item>
+                </v-list>
+            </template>
+            <template v-slot:no-results>
+                <span>Nenhum resultado encontrado</span>
+            </template>
+        </v-data-iterator>
+    </v-container>
 </template>
 
 <script>
-    import ListaIDC from '@/components/ListaIDC.vue';
+    import ItemIDC from '@/components/ItemIDC.vue';
+    const myIdcs = [
+        {id: 1, nome: 'Igreja São João', dataDistribuicao: "27/05/2022", cestasRestantes: 3, composicaoCesta: ['Arroz', 'Feijão']},
+        {id: 2, nome: 'Projeto lá de casa', dataDistribuicao: "27/05/2022", cestasRestantes: 1, composicaoCesta: ['Arroz', 'Feijão']}
+    ]
     export default {
         data() {
-            return {}
+            return {
+                idcs: myIdcs,
+                search: '',
+                filter: {}
+            }
         },
         components: {
-            ListaIDC
+            ItemIDC
         }
     }
 </script>
