c4cf38eedcf7483abd6a36cc9136cdf2f343ff5c
Txai <txai.garcia@gmail.com>
2022-10-03 21:07:08 -0300

    Inicializando serviço de iniciativas

---
+++ servicoiniciativas/src/main/java/com/receba/servicoiniciativas/ServicoiniciativasApplication.java
@@ -0,0 +1,13 @@
+package com.receba.servicoiniciativas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoiniciativasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoiniciativasApplication.class, args);
+	}
+
+}
---
+++ servicoiniciativas/src/test/java/com/receba/servicoiniciativas/ServicoiniciativasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.receba.servicoiniciativas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoiniciativasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
